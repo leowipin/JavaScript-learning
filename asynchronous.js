b60eb7eae621bd0b659a860bc6af21ee8a22ff85
leowipin <leowipin@espol.edu.ec>
2022-04-20 14:50:21 -0500

    .then

--- asynchronous.js
+++ asynchronous.js
@@ -5,7 +5,7 @@ const stock =
     holder: ["cone", "cup"],
     toppings: ["chocapic", "oreo", "candy"]
 };
-let is_open = false;
+let is_open = true;
 
 let order = (time, work) => {
     return new Promise((resolve, reject) => { //search more about promise
@@ -20,4 +20,29 @@ let order = (time, work) => {
     });
 };
 
-order(2000, () => console.log(stock.fruit[0]+" was selected"));
+order(2000, () => console.log(stock.fruit[0]+" was selected"))
+
+.then(()=>{
+    return order(2000,()=> console.log("The production has started"));
+})
+
+.then(()=>{
+    return order(2000,()=> console.log("The "+stock.fruit[0]+" was chopped"));
+})
+
+.then(()=>{
+    return order(1000,()=> console.log(stock.liquid[0]+" and "+stock.liquid[1]+" was added "));
+})
+
+.then(()=>{
+    return order(1000,()=> console.log("start the machine"));
+})
+
+.then(()=>{
+    return order(3000,()=> console.log("ice cream placed on "+stock.holder[0]));
+})
+
+.then(()=>{
+    return order(2000,()=> console.log("Ice cream was served"));
+})
+
